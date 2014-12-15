c20d720950f378c46cdbed5838bcbebbb507442c
Brian Holt <brian.c.holt@gmail.com>
2014-12-15 13:39:58 -0800

    updaing quiz 3

--- quiz3.R
+++ quiz3.R
@@ -14,6 +14,42 @@ setwd("I:\\My Data Sources\\mooc\\GetCleanData")
 
 if (!file.exists("data")) {  dir.create("data")}
 
-download.file("http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv",destfile="./data/Housing.csv")
-#("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf" for variable desc
-house<-read.csv("./data/Housing.csv")
+#q1
+download.file("http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv",destfile="./data/idahoHousing.csv")
+#https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf 
+idaho<-read.csv("./data/idahoHousing.csv")
+
+idaho[which(idaho$ACR==3 & idaho$AGS == 6),c("ACR","AGS")]
+idaho$agricultureLogical<-(c(idaho$ACR==3 & idaho$AGS == 6))
+which(idaho$agricultureLogical)
+
+idaho[order(idaho$NP),]
+
+
+#q2
+library("jpeg")
+
+download.file("http://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg",destfile="./data/leek.jpg")
+#https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf 
+leekPhoto<-readJPEG("./data/leek.jpg",native =T)
+quantile(leekPhoto,probs=seq(0,1,.1))
+
+
+#q3
+download.file("http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv",destfile="./data/gdp.csv")
+#https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf 
+gdp<-read.csv("./data/gdp.csv")
+
+download.file("http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv",destfile="./data/edu.csv")
+#https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf 
+edu<-read.csv("./data/edu.csv")
+
+head(gdp)
+names(edu)
+
+table(edu[,31] %in% gdp[,"X.2"])
+table(edu[,1] %in% gdp[,1])
+head(gdp)
+df<-merge(edu,gdp,by.y="X.2", by.x="Short.Name")
+names(df)
+df[order(df[,33],decreasing=T),]
